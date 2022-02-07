8c98b8e8fc0ce1bcbd88c5b0cc31fb019b5c7a51
ahmedmohab96 <ahmdmhabhbyb@gmail.com>
2022-02-08 01:43:07 +0200

    https://github.com/ahmedmohab96/Admin_Pannar_app.git

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:admin_app/screens/main/main_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'constans.dart';
+import 'controllers/menu_controller.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    // ignore: unused_local_variable
+    var mainScreen = MainScreen;
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Admin Panel',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: bgColor,
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
+            .apply(bodyColor: Colors.white),
+        canvasColor: secondaryColor,
+      ),
+      home: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+            create: (context) => MenuController(),
+          ),
+        ],
+        child: MainScreen(),
+      ),
+    );
+  }
+}
